b42f268b21d4b84b584d1f2d088a3bfff7cc0ccc
ASiddiqui <aftabsiddiqui3@gmail.com>
2018-03-20 12:04:05 -0400

    second path creation

---
+++ project1/src/HomeWork1/Name_Id.java
@@ -0,0 +1,13 @@
+package HomeWork1;
+
+public class Name_Id 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Name: Aftab Siddiqui");
+		System.out.println("  Id: 3653_NY");
+
+	}
+
+}
---
+++ project1/src/hello/HelloSelenium.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class HelloSelenium {
+    static String a = "aftab";
+
+	public static void main(String[] args) {
+		HelloSelenium bla = new HelloSelenium();
+			bla = a;
+		System.out.println(bla);
+
+	}
+
+}
